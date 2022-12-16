e93ae737fa3cf4feff8462db2673b4cef68033eb
Difenter <afonking.denis13@gmail.com>
2022-12-16 11:12:01 +0200

    fix: write concern = UNACKNOWLEDGED

--- src/main/kotlin/data/utils/MongoDbProvider.kt
+++ src/main/kotlin/data/utils/MongoDbProvider.kt
@@ -8,7 +8,7 @@ import com.mongodb.client.MongoClients
 object MongoDbProvider {
 
     private val client = MongoClients.create(MongoClientSettings.builder()
-        .writeConcern(WriteConcern.W1)
+        .writeConcern(WriteConcern.UNACKNOWLEDGED)
         .applyConnectionString(ConnectionString(
             "mongodb://127.0.0.1:27001,127.0.0.1:27002,127.0.0.1:27003"
         ))
